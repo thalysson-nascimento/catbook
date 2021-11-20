f1b447386b42e7b640151524a6b123d7ca2d07e0
Thalysson <thalyssonwg.nascimento@gmail.com>
2021-11-20 12:07:17 -0300

    chore: included modules home

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, LoginComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule
   ],
   exports: [HomeComponent]
 })
