5a860fd4bd265eb70ead9b979163f6f14d733ce6
Thalysson <thalyssonwg.nascimento@gmail.com>
2021-11-21 17:11:38 -0300

    feat: included reactive forms module

--- src/app/pages/animals/animals.module.ts
+++ src/app/pages/animals/animals.module.ts
@@ -1,7 +1,7 @@
-import { AnimalsComponent } from './animals.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AnimalsComponent } from './animals.component';
 import { AnimalsRoutingModule } from './animals-routing.module';
 
 
