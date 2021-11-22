e3b8151345942289fb06e9ffe466a66a1eb3fb3a
Thalysson <thalyssonwg.nascimento@gmail.com>
2021-11-21 21:53:27 -0300

    chore: declaration component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,4 +1,3 @@
-import { NewUserComponent } from './new-user/new-user.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -6,9 +5,16 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { MessageComponent } from './../../components/message/message/message.component';
+import { NewUserComponent } from './new-user/new-user.component';
 
 @NgModule({
-  declarations: [HomeComponent, LoginComponent, NewUserComponent],
+  declarations: [
+    HomeComponent,
+    LoginComponent,
+    NewUserComponent,
+    MessageComponent,
+  ],
   imports: [
     CommonModule,
     FormsModule,
