082d4734fe6c561a7442c28b607bc41314761241
Thalysson <thalyssonwg.nascimento@gmail.com>
2021-12-04 12:55:54 -0300

    feat: included components

--- src/app/pages/animals/animals.component.ts
+++ src/app/pages/animals/animals.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./animals.component.css']
 })
 export class AnimalsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
